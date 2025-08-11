0df4fcc072e1a4ff410c165a0c8982ef72f899f8
lnylnylnylny <lnylnylnylny@naver.com>
2025-08-11 17:11:01 +0900

    chore: asset 주입

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,46 @@
+// logo
+export { default as Timemory_logo } from "./icons/Timemory_logo.svg";
+export { default as mini_logo } from "./icons/mini_logo.svg";
+
+// Icons
+export { default as back_arrow } from "./icons/back_arrow.svg";
+export { default as bell } from "./icons/bell.svg";
+export { default as calendar } from "./icons/calendar.svg";
+export { default as camera } from "./icons/camera.svg";
+export { default as kakao_logo } from "./icons/kakao_logo.svg";
+export { default as lightbulb } from "./icons/lightbulb.svg";
+export { default as lock } from "./icons/lock.svg";
+export { default as map_pin } from "./icons/map_pin.svg";
+export { default as option } from "./icons/option.svg";
+
+// Images
+export { default as capsule_img } from "./images/capsule_img.svg";
+export { default as hashtag_img } from "./images/hashtag_img.svg";
+export { default as cloud_img } from "./images/cloud_img.svg";
+export { default as camera_img } from "./images/camera_img.svg";
+export { default as condition_img } from "./images/condition_img.svg";
+export { default as map_img } from "./images/map_img.svg";
+export { default as profile_img } from "./images/profile_img.svg";
+export { default as profile_img2 } from "./images/profile_img2.svg";
+
+// Event Day
+export { default as april_fools_day } from "./images/april_fools_day.svg";
+export { default as april_fools_day2 } from "./images/april_fools_day2.svg";
+export { default as pepero_day } from "./images/pepero_day_img.svg";
+export { default as pepero_day2 } from "./images/pepero_day_img2.svg";
+export { default as pepero_day3 } from "./images/pepero_day_img3.svg";
+export { default as white_day } from "./images/white_day_img.svg";
+export { default as white_day2 } from "./images/white_day_img2.svg";
+export { default as valentines_day } from "./images/valentines_day_img.svg";
+export { default as valentines_day2 } from "./images/valentines_day_img2.svg";
+export { default as new_year } from "./images/new_year_img.svg";
+export { default as new_year2 } from "./images/new_year_img2.svg";
+export { default as new_year3 } from "./images/new_year_img3.svg";
+export { default as christmas } from "./images/christmas_img.svg";
+export { default as christmas2 } from "./images/christmas_img2.svg";
+
+// Tab bar Icons
+export { default as group_tab } from "./icons/group_tab.svg";
+export { default as home_tab } from "./icons/home_tab.svg";
+export { default as mypage_tab } from "./icons/mypage_tab.svg";
+export { default as capsule_tab } from "./icons/capsule_tab.svg";
