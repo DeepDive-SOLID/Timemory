7dd258fd36d4345d6f95bc14be90ac5d6aee407e
w-jins <jineen@naver.com>
2025-08-19 16:15:10 +0900

    feat: map 관련 redux 생성

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import locationSlice from "./locationSlice";
+import searchTypeSlice from "./searchTypeSlice"
 
 export const store = configureStore({
   reducer: {
-    // Add your reducers here
+    location: locationSlice,
+    searchType: searchTypeSlice,
   },
 });
 
