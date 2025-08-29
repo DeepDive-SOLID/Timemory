4f2cccf3fd4792ea92a17e2c080c730bc55f5e43
w-jins <jineen@naver.com>
2025-08-29 13:37:52 +0900

    fix: 요청 url 변경

--- frontend/src/api/MapApi.ts
+++ frontend/src/api/MapApi.ts
@@ -1,14 +1,14 @@
 import api from "./axios";
 import type { LtAddDto, LtListDto } from "../types/map";
 
-export const getLtList = async (teamId: number) => {
+export const getLtListApi = async (teamId: number) => {
   const res = await api.post<LtListDto[]>("/capsule/lt/getLtList", teamId, {
     headers: { "Content-Type": "application/json" },
   });
   return res.data;
 };
 
-export const saveLt = async (ltDto: LtAddDto) => {
+export const saveLtApi = async (ltDto: LtAddDto) => {
   const res = await api.post("/capsule/lt/addLtDto", ltDto);
 
   return res.data;
