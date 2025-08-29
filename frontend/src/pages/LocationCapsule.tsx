3af1f4f758b5697bb167486c967db51b779ddd4a
w-jins <jineen@naver.com>
2025-08-29 13:37:17 +0900

    refactor: 코드 리펙토링

--- frontend/src/pages/LocationCapsule.tsx
+++ frontend/src/pages/LocationCapsule.tsx
@@ -1,40 +1,14 @@
-import { useEffect, useMemo, useState } from "react";
 import Kakao from "../components/Domain/Map/Kakao";
-import { getLtList } from "../api/MapApi";
-import useLatLng from "../hooks/useLatLng";
-import type { LtListDto } from "../types/map";
 import Back from "../components/Domain/Map/Back";
+import useCapsuleData from "../hooks/useCapsuleData";
 
 const LocationCapsule = () => {
-  const [data, setData] = useState<LtListDto[]>([]);
-  const addressList = useMemo(() => data.map((item) => item.capLtAddr), [data]);
-  const latLng = useLatLng({ input: addressList });
-
-  useEffect(() => {
-    const res = async () => {
-      try {
-        const resData = await getLtList(1);
-        setData(resData);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    res();
-  }, []);
-
-  const enrichedData = useMemo(() => {
-    if (!latLng || latLng.length === 0) return data;
-
-    return data.map((item) => {
-      const found = latLng.find((loc) => loc.addr === item.capLtAddr);
-      return found ? { ...item, lat: found.lat, lng: found.lng } : item;
-    });
-  }, [data, latLng]);
-
+  // 컴포넌트에 필요한 데이터를 요청하는 커스텀 훅
+  const data = useCapsuleData();
   return (
     <>
       <Back />
-      <Kakao customProps={enrichedData} />
+      <Kakao customProps={data} />
     </>
   );
 };
